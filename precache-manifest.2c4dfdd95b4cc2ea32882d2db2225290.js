a02b525b7be3e656389202d172321a0d0447cd45
TwelveGEater <dronkfc@gmail.com>
2020-05-01 01:47:37 +0300

    Updates

---
+++ precache-manifest.2c4dfdd95b4cc2ea32882d2db2225290.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06e94b6ffbba5bc8e8991c1976172ef",
+    "url": "/React-Recipe-Search-App/index.html"
+  },
+  {
+    "revision": "dce45c51c3cda68c806e",
+    "url": "/React-Recipe-Search-App/static/css/main.b507ec05.chunk.css"
+  },
+  {
+    "revision": "25715a76134517e7670f",
+    "url": "/React-Recipe-Search-App/static/js/2.e464cae5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Recipe-Search-App/static/js/2.e464cae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce45c51c3cda68c806e",
+    "url": "/React-Recipe-Search-App/static/js/main.92adf570.chunk.js"
+  },
+  {
+    "revision": "84a5ac118e5633d0fa40",
+    "url": "/React-Recipe-Search-App/static/js/runtime-main.89f02f71.js"
+  },
+  {
+    "revision": "05aa77b14a3e3c5a79b60b00b34e9652",
+    "url": "/React-Recipe-Search-App/static/media/BalooTamma2-Regular.05aa77b1.ttf"
+  }
+]);
