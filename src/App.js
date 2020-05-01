b241dc406f3153a80f2fc4d35e0b7e88cb6bc01d
TwelveGEater <dronkfc@gmail.com>
2020-05-01 12:05:11 +0300

    fixed string

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
 	const [ recipes, setRecipes ] = useState([]);
 	const [ search, setSearch ] = useState('');
-	const [ query, setQuery ] = useState('chicken');
+	const [ query, setQuery ] = useState('sweet');
 
 	useEffect(
 		() => {
